26a82354e6dfe441b0ad4694fc59c61c0ce584d7
Carlos Contreras <carlos.conpe@hotmail.com>
2021-02-26 12:39:01 -0600

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
+
+
+
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
